be9715604f421ffdf7a6e238401e593c8a100310
dathmhe141249 <80512109+dathmhe141249@users.noreply.github.com>
2021-08-04 22:44:40 +0700

    Add files via upload

---
+++ C#/C# Fundamental/staticKeyword/Ultility.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace staticKeyword
+{
+    //mục tiêu class static: Class này chứa các thành phần đc sd chung trong nhiều class khác
+    class Ultility
+    {
+        // trong class static chỉ đc chứa: constant, properties, static methods
+        public static string name;
+        public const bool truth = true;
+
+        public static void PrintTime()
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+        }
+
+        public static void PrintDate()
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+        }
+
+
+    }
+}
