4f6dfc063040f2d5b969d39d65ce0160f17a100a
dathmhe141249 <80512109+dathmhe141249@users.noreply.github.com>
2021-08-04 17:05:09 +0700

    Add files via upload

---
+++ C#/staticKeyword/AccountSaving.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace staticKeyword
+{
+    class AccountSaving
+    {
+        public double balance;
+        public static double interestRate;
+
+       
+
+        public AccountSaving(double balance)
+        {
+            this.balance = balance;
+        }
+
+        // sẽ đc gọi tự động khi có 1 đối tượng kiểu AccountSaving đc tạo ra
+        // chỉ đc gọi 1 lần duy nhất
+        static AccountSaving()
+        {
+            Console.WriteLine("Static constructor called when");
+            interestRate = 0.04;
+        }
+
+        public static void setInterestRate(double newRate)
+        {
+            interestRate = newRate;
+        }
+        public static double getInterestRate()
+        {
+            return interestRate;
+        }
+        public void setInterestRateObj(double newRate)
+        {
+            interestRate = newRate;
+        }
+        public  double getInterestRateObj()
+        {
+            return interestRate;
+        }
+    }
+}
