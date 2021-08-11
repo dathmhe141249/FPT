35f68ca087c2106c139b6c26c9edbe8c6159a6fc
dathmhe141249 <80512109+dathmhe141249@users.noreply.github.com>
2021-08-11 17:31:58 +0700

    Add files via upload

---
+++ C#/Team Project Asp .Net core 3.1/DAL Database/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration.Json;
+
+
+namespace DAL_Database
+{
+    public class DBConnection
+    {
+        public string GetConnectionString()
+        {
+            //Install 2 Libraries are Microsoft.Extensions.Configuration and Microsoft.Configuration.Json
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true);
+            return builder.Build().GetSection("ConnectionStrings").GetSection("DefaultConnecting").Value;
+        }
+    }
+}
