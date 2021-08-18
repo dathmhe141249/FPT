465d2f77e1c1417161e3bf81c49cd2d7cf44ca54
dathmhe141249 <80512109+dathmhe141249@users.noreply.github.com>
2021-08-18 23:40:49 +0700

    Add files via upload

---
+++ C#/Hacker Rank/Main/Drawing_Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+    class Drawing_Book
+    {
+        public static int pageCount(int n, int p)
+        {
+            int TargetPageCountFromFront = p / 2;
+            int totalFlipFromFrontToBack = n / 2;
+            int TargetPageCountFromBack = totalFlipFromFrontToBack - TargetPageCountFromFront;
+            return Math.Min(TargetPageCountFromFront, TargetPageCountFromBack);
+        }
+    }
+}
---
+++ C#/Hacker Rank/Main/Sales_By_Match.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Main
+{
+    class Sales_By_Match
+    {
+        public static int sockMerchant(int n, List<int> ar)
+        {
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (ar[i] != 0)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+
+                        if (ar[i] == ar[j])
+                        {
+                            count++;
+                            ar[j] = 0; // ~ a[i+1] = 0 vì nếu xét rồi, sẽ k xét nữa vì k tính
+                            break;
+                        }
+                    }
+                }
+
+            }
+            return count;
+        }
+    }
+}
